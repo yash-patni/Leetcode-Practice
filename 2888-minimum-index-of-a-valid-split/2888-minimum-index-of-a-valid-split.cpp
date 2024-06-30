4b7f71cdc5f8693da835dd804a2a117e04a65627
Yash Patni <121944122+yash-patni@users.noreply.github.com>
2024-07-01 01:48:58 +0530

    Time: 139 ms (61.45%), Space: 97.9 MB (57.83%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -3,9 +3,12 @@ public:
     int minimumIndex(vector<int>& nums) {
         int m=nums.size();
         unordered_map<int, int> freq;
+        //counting frequency of every element
         for(int i=0;i<m;i++){
             freq[nums[i]]++;
         }
+        // storing num in element having freq>m/2 i.e 2*freq>m
+        //and freq of num in total
         int element, total;
         for(auto it:freq){
             if(it.second>m/2){
@@ -14,10 +17,10 @@ public:
                 break;
             }
         }
-        int count=0;
+        int count=0;//declared count to keep count of left arr(0 to i) index
         for(int i=0;i<m;i++){
-            int l=i+1;
-            int r=m-1-i;
+            int l=i+1;//left part size
+            int r=m-1-i;//right part size
             if(nums[i]==element) count++;
             if(count*2>l && (total-count)*2>r){
                 return i;
