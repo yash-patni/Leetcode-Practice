44fef720d2d95010300f39f70f0eee80cc0f8ab1
Yash Patni <121944122+yash-patni@users.noreply.github.com>
2024-07-23 00:34:28 +0530

    Time: 33 ms (40.77%), Space: 27.1 MB (46.5%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.cpp
+++ 2502-sort-the-people/2502-sort-the-people.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        // //BUBBLE SORT
+        // int n=heights.size();
+        // bool swapped;
+        // for(int i=0;i<n-1;i++){
+        //     swapped=false;
+        //     for(int j=0;j<n-i-1;j++){
+        //         if(heights[j]<heights[j+1]){
+        //             swap(heights[j], heights[j+1]);
+        //             swap(names[j], names[j+1]);
+        //             swapped=true;
+        //         }
+        //     }
+        //     if(swapped==false){
+        //         break;
+        //     }
+        // }
+        // // reverse(names.begin(), names.end());
+        // return names;
+        // // T.C. : O(n^2) S>C : O(1)
+
+        //Optimal Approach
+        vector<pair<int,string>>ans;
         int n=heights.size();
-        bool swapped;
-        for(int i=0;i<n-1;i++){
-            swapped=false;
-            for(int j=0;j<n-i-1;j++){
-                if(heights[j]<heights[j+1]){
-                    swap(heights[j], heights[j+1]);
-                    swap(names[j], names[j+1]);
-                    swapped=true;
-                }
-            }
-            if(swapped==false){
-                break;
-            }
+        for(int i=0;i<n;i++)
+        {
+            ans.push_back({heights[i],names[i]});
         }
-        // reverse(names.begin(), names.end());
-        return names;
+        vector<string>res;
+        sort(ans.begin(),ans.end());
+        for(int i=n-1;i>=0;i--)
+        {
+            res.push_back(ans[i].second);
+        }
+        return res;
     }
 };
