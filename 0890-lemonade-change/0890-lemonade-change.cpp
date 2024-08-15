aa68ec89258f751b826b0b584eac168c9df9dff3
Yash Patni <121944122+yash-patni@users.noreply.github.com>
2024-08-15 12:21:24 +0530

    Time: 85 ms (9.35%), Space: 86 MB (32.71%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -7,17 +7,17 @@ public:
         if(bills[0]!=5) return false;
         for(int i=0;i<n;i++){
             if(bills[i]==5){
-                count5+=1;
+                count5++;
             }
             else if(bills[i]==10){
                 if(count5<1) return false;
-                count5-=1;
-                count10+=1;
+                count5--;
+                count10++;
             }
             else{
                 if(count10>0 && count5>0){
-                    count10-=1;
-                    count5-=1;
+                    count10--;
+                    count5--;
                 }
                 else if(count5>2){
                     count5-=3;
