eebb840142621e2ec3109594720dd7a6499c6e28
Yash Patni <121944122+yash-patni@users.noreply.github.com>
2024-08-21 08:01:10 +0530

    Time: 14 ms (70.32%), Space: 12.3 MB (61.31%) - LeetHub

---
+++ 1240-stone-game-ii/1240-stone-game-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n=piles.size();
+        vector<vector<int>> dp(n, vector<int>(n+1, 0));
+        vector<int> suffSum(n,-1);
+        int sum=0;
+        for(int i=n-1;i>=0;i--){
+            sum+=piles[i];
+            suffSum[i]=sum;
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int m=1;m<=n;m++){
+                if(i+2*m>=n){
+                    dp[i][m]=suffSum[i];
+                }
+                else{
+                    for(int x=1;x<=2*m;x++){
+                        dp[i][m]=max(dp[i][m], suffSum[i]-dp[i+x][max(m,x)]);
+                    }
+                }
+            }
+        }
+        return dp[0][1];
+    }
+};
