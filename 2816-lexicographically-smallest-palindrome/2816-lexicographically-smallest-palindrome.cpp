8cbb0c6090e9a3b4912c1def2d76190d0e504fc1
Yash Patni <121944122+yash-patni@users.noreply.github.com>
2024-09-23 23:50:04 +0530

    Time: 32 ms (69.21%), Space: 18.7 MB (84.26%) - LeetHub

---
+++ 2816-lexicographically-smallest-palindrome/2816-lexicographically-smallest-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int n = s.size();
+        int left = 0;
+        int right = n-1;
+        while(left < right)
+        {
+            if(s[left] != s[right])
+            {
+                char most = (s[left] < s[right]) ? s[left] : s[right];
+                s[left] = most;
+                s[right] = most;
+            }
+            left++;
+            right--;
+        }
+        return s;
+    }
+};
