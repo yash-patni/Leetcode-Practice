feca0d47c29e16452bfcd9ebce1f69fe6a998bed
Yash Patni <121944122+yash-patni@users.noreply.github.com>
2024-10-09 11:55:18 +0530

    Time: 43 ms (54.33%), Space: 16.4 MB (79.06%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -4,11 +4,12 @@ class Solution:
         i=n-2
         while i>=0 and nums[i]>=nums[i+1]:
             i-=1
-
+        
         if i>=0:
             j=n-1
             while nums[j]<=nums[i]:
                 j-=1
-            nums[i],nums[j]=nums[j],nums[i]
-
+            
+            nums[j], nums[i] = nums[i], nums[j]
+        
         nums[i+1:]=reversed(nums[i+1:])
