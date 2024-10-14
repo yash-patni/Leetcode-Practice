67725b05fc3d25a4c955c4b8d6594cd5a83b0130
Yash Patni <121944122+yash-patni@users.noreply.github.com>
2024-10-14 23:45:47 +0530

    Time: 7 ms (40.84%), Space: 18.1 MB (22.49%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,24 +1,45 @@
 class Solution {
 public:
-    int f(int ind, int buy, int n, vector<int>& prices, vector<vector<int>>& dp){
-        if(ind==n){
-            return 0;
-        }
-        if(dp[ind][buy]!=-1) return dp[ind][buy];
-        int take, notake;
-        if(buy){
-            take=-prices[ind]+f(ind+1, 0, n, prices, dp);
-            notake=0+f(ind+1, 1, n, prices, dp);
-        }
-        else{
-            take=prices[ind]+f(ind+1, 1, n, prices, dp);
-            notake=0+f(ind+1, 0, n, prices, dp);
-        }
-        return dp[ind][buy]=max(take, notake);
-    }
+    //MEMOISATION
+    // int f(int ind, int buy, int n, vector<int>& prices, vector<vector<int>>& dp){
+    //     if(ind==n){
+    //         return 0;
+    //     }
+    //     if(dp[ind][buy]!=-1) return dp[ind][buy];
+    //     int take, notake;
+    //     if(buy){
+    //         take=-prices[ind]+f(ind+1, 0, n, prices, dp);
+    //         notake=0+f(ind+1, 1, n, prices, dp);
+    //     }
+    //     else{
+    //         take=prices[ind]+f(ind+1, 1, n, prices, dp);
+    //         notake=0+f(ind+1, 0, n, prices, dp);
+    //     }
+    //     return dp[ind][buy]=max(take, notake);
+    // }
+    // int maxProfit(vector<int>& prices) {
+    //     int n=prices.size();
+    //     vector<vector<int>> dp(n, vector<int>(2,-1));
+    //     return f(0, 1, n, prices, dp);
+    // }
+
+    //TABULATION
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> dp(n, vector<int>(2,-1));
-        return f(0, 1, n, prices, dp);
+        vector<vector<int>> dp(n+1, vector<int>(2,0));
+        dp[n][0]=dp[n][1]=0;
+        for(int ind=n-1; ind>=0; ind--){
+            for(int buy=0; buy<=1; buy++){
+                int profit=0;
+                if(buy){
+                    profit=max(-prices[ind]+dp[ind+1][0], 0+dp[ind+1][1]);
+                }
+                else{
+                    profit=max(prices[ind]+dp[ind+1][1], 0+dp[ind+1][0]);
+                }
+                dp[ind][buy]=profit;
+            }
+        }
+        return dp[0][1];
     }
 };
