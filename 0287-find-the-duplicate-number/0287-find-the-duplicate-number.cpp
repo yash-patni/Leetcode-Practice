3d10b0d266af5c9738b1d151ea034fa9592337bb
Yash Patni <121944122+yash-patni@users.noreply.github.com>
2024-10-16 18:03:02 +0530

    Time: 81 ms (54.14%), Space: 63.8 MB (82.46%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,26 +1,53 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        int low=1;
-        int high=n-1;
-        int ans=0;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            int cnt=0;
-            for(int i=0;i<n;i++){
-                if(nums[i]<=mid){
-                    cnt++;
-                }
-            }
-            if(cnt<=mid){
-                low=mid+1;
-            }
-            else{
-                ans=mid;
-                high=mid-1;
-            }
+        // Binary search Approach
+        // int n=nums.size();
+        // int low=1;
+        // int high=n-1;
+        // int ans=0;
+        // while(low<=high){
+        //     int mid=low+(high-low)/2;
+        //     int cnt=0;
+        //     for(int i=0;i<n;i++){
+        //         if(nums[i]<=mid){
+        //             cnt++;
+        //         }
+        //     }
+        //     if(cnt<=mid){
+        //         low=mid+1;
+        //     }
+        //     else{
+        //         ans=mid;
+        //         high=mid-1;
+        //     }
+        // }
+        // return ans;
+        // TC: O(NlogN) //SC:O(1)
+        
+        //slow fast approach
+        //cycle flow
+        //for test case 1:
+        // Input: nums = [1,3,4,2,2]
+        // Output: 2
+        //0->1->3->2->4->2 here there is a cycle between 2-4-2
+        int slow=0, fast=0;
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];  
+        }while(slow!=fast);
+
+        int sp=0;//starting point
+        int mp=fast;//meeting point of slow and fast
+        while(sp!=mp){
+            sp=nums[sp];
+            mp=nums[mp];
         }
-        return ans;
+        return sp;
     }
+    
+    
+
+    
+     
 };
