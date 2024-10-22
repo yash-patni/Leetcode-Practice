3c0d351bddd0d2e0a5237e601ee78c8a91dd2e2b
Yash Patni <121944122+yash-patni@users.noreply.github.com>
2024-10-22 16:28:38 +0530

    Time: 0 ms (100%), Space: 10.2 MB (33.93%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -10,16 +10,18 @@
  * };
  */
 class Solution {
-public: 
-    void inorder(TreeNode* root, vector<int>& ans){
-        if(root==NULL) return;
-        inorder(root->left, ans);
+public:
+    void inorder(vector<int>& ans, TreeNode* root){
+        if(root==NULL){
+            return;
+        }
+        inorder(ans, root->left);
         ans.push_back(root->val);
-        inorder(root->right, ans);
+        inorder(ans, root->right);
     }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        inorder(root, ans);
+        inorder(ans, root);
         return ans;
     }
 };
