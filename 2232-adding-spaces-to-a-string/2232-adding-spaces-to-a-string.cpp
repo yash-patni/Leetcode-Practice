ca05e9d0b66f1fd42269c0a0e7884ed768ae4b2a
Yash Patni <121944122+yash-patni@users.noreply.github.com>
2024-12-03 20:15:13 +0530

    Time: 15 ms (85.84%), Space: 82.3 MB (89.15%) - LeetHub

--- 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.cpp
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
     string addSpaces(string s, vector<int>& spaces) {
-        vector<string> words;
-        int n=spaces.size();
-        string w="";
+        //Approach 2
+        int n=s.length();
+        int m=spaces.size();
+        string ans(n+m, ' ');
         int j=0;
-        for(int i=0; i<s.length(); i++){
-            if(j < n && i == spaces[j]) {
-                words.push_back(w);
-                w = "";
+        for(int i=0; i<n; i++){
+            if(j<m && i==spaces[j]){
+                ans[i+j]=' ';
                 j++;
             }
-            w += s[i];
-        }
-        words.push_back(w);
-        int l=words.size();
-        string ans=words[0];
-        for(int i=1; i<l; i++){
-            ans+=" ";
-            ans+=words[i];
+            ans[i+j]=s[i];
         }
         return ans;
 
+        //Approach 1
+        // vector<string> words;
+        // int n=spaces.size();
+        // string w="";
+        // int j=0;
+        // for(int i=0; i<s.length(); i++){
+        //     if(j < n && i == spaces[j]) {
+        //         words.push_back(w);
+        //         w = "";
+        //         j++;
+        //     }
+        //     w += s[i];
+        // }
+        // words.push_back(w);
+        // int l=words.size();
+        // string ans=words[0];
+        // for(int i=1; i<l; i++){
+        //     ans+=" ";
+        //     ans+=words[i];
+        // }
+        // return ans;
     }
 };
