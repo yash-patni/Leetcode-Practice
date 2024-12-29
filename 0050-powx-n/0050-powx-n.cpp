b1ce13db8abc87f7fc248c2acd73aa4c885a3e35
Yash Patni <121944122+yash-patni@users.noreply.github.com>
2024-12-29 14:25:13 +0530

    Time: 0 ms (100%), Space: 8.5 MB (42.51%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-    double solve(double x, int n){
-        if(x==0 || x==1) return x;
-        if(n==0) return 1;
-
-        double ans=solve(x, n/2);
-        ans=ans*ans;
-        if(n%2){
-            return ans*x;
-        }
-        else{
-            return ans;
-        }
-    }
     double myPow(double x, int n) {
-        //BRUTE FORCE
-        // double ans=1;
-        // if(x==0 || x==1) return x;
-        // if(n==0) return ans;
-        // for(int i=0;i<abs(n);i++){
-        //     ans*=x;
-        // }
-        // if(n<0) return 1/ans;
-        // return ans;
-
-        //ANOTHER APPROACH
-        double ans=solve(x, abs(n));
-        if(n<0) return 1/ans;
+        double ans=1.0;
+        long long p=n;
+        if(p<0) p=-1*p;
+        while(p){
+            if(p%2){
+                ans=ans*x;
+                p=p-1;
+            }
+            else{
+                x=x*x;
+                p=p/2;
+            }
+        }
+        if(n<0) ans=(double)(1.0)/(double)(ans);
         return ans;
     }
 };
